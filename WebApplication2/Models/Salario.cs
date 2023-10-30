c1bb5d4b036673600a602749fcf48654a894b277
carlosraulcala <calacamatacarlosraul@gmail.com>
2023-10-30 10:50:39 -0400

    salario

--- WebApplication2/Models/Salario.cs
+++ WebApplication2/Models/Salario.cs
@@ -5,7 +5,7 @@ namespace WebApplication2.Models
     public class Salario
     {
         [Key]
-        public int idSalario { get; set; }
+        public string idSalario { get; set; }
         public string salario { get; set; }
         public string FechaInicio { get; set; }
     }
